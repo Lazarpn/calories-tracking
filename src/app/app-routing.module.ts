de1d9d9270c32040c924b53655998e001406a134
Lazar <lazarst.pn@gmail.com>
2022-11-03 15:32:49 +0100

    Adding route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { MealsComponent } from './meals/meals.component';
 
-const routes: Routes = [{ path: 'auth', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: MealsComponent },
+  { path: 'auth', component: AuthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
